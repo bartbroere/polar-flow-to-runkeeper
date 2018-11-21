101997ec9041ad2a7e3521070df2c2f7f374c3a5
Bart Broere <broerebart@hotmail.com>
2018-11-21 17:03:52 +0100

    Update handler.py

--- handler.py
+++ handler.py
@@ -11,6 +11,10 @@ def run(event, context):
     # email: email
     # password: password
     # returnUrl: '/'
+    
+    # https://runkeeper.com/login
+    # POST
+    # _eventName=submit&redirectUrl=&flow=&failUrl=&secureParams=&email=email%40email.com&password=undisclosed&_sourcePage=wiAO6roZofdSMkQ2IKb_mRoc-IQ0sMyrmpVzZ9KGq7kaHPiENLDMq5qVc2fShqu5knVNNT0OC_8%3D&__fp=WAvsyf4_Zig%3D
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
