ee492032c6d7b0598020ddc263a4c3d283c4a8c3
Bart Broere <broerebart@hotmail.com>
2018-11-21 16:45:03 +0100

    Update handler.py

--- handler.py
+++ handler.py
@@ -6,6 +6,11 @@ logger.setLevel(logging.INFO)
 
 
 def run(event, context):
+    # https://flow.polar.com/login
+    # POST
+    # email: email
+    # password: password
+    # returnUrl: '/'
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
