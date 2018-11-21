1aca0570cdbd82230a7dcab59136ca58b0212126
Bart Broere <broerebart@hotmail.com>
2018-11-21 17:24:08 +0100

    Authenticate with Polar Flow and Runkeeper

--- handler.py
+++ handler.py
@@ -1,20 +1,50 @@
 import datetime
 import logging
 
+import requests
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+class PolarFlowClient:
+
+    def __init__(self):
+        self.session = requests.Session()
+
+    def login(self, username, password):
+        self.session.post('https://flow.polar.com/login',
+                          data={"email": username,
+                                "password": password,
+                                "returnUrl": '/'})
+
+
+class RunkeeperClient:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.post = self.session.post
+        self.get = self.session.get
+
+    def login(self, username, password):
+        self.session.post('https://runkeeper.com/login',
+                          data={'_eventName': 'submit',
+                                'redirectUrl': '',
+                                'flow': '',
+                                'failUrl': '',
+                                'secureParams': '',
+                                'email': username,
+                                'password': password,
+                                '_sourcePage': 'wiAO6roZofdSMkQ2IKb_mRoc-'
+                                               'IQ0sMyrmpVzZ9KGq7kaHPiENL'
+                                               'DMq5qVc2fShqu5knVNNT0OC_8'
+                                               '%3D',
+                                '_fp': 'WAvsyf4_Zig%3D'}
+                          )
+
+
 def run(event, context):
-    # https://flow.polar.com/login
-    # POST
-    # email: email
-    # password: password
-    # returnUrl: '/'
-    
-    # https://runkeeper.com/login
-    # POST
-    # _eventName=submit&redirectUrl=&flow=&failUrl=&secureParams=&email=email%40email.com&password=undisclosed&_sourcePage=wiAO6roZofdSMkQ2IKb_mRoc-IQ0sMyrmpVzZ9KGq7kaHPiENLDMq5qVc2fShqu5knVNNT0OC_8%3D&__fp=WAvsyf4_Zig%3D
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
+    # TODO ETL here
